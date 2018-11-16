a31f6c399d524b4af12dd4498a44c0f5675e3e6b
alvi2496 <alvi.uvic@gmail.com>
2018-11-15 16:45:00 -0800

    initial data processing

--- app/data/input.py
+++ app/data/input.py
@@ -1,4 +1,6 @@
-import pandas as pd
+import pandas
+from sklearn import model_selection
+from sklearn.feature_extraction.text import CountVectorizer
 
 def input_file_path():
 	# data_file_path = input("Path to data file: ")
@@ -7,5 +9,20 @@ def input_file_path():
 	return data_file_path
 
 def structure(data_file_path):
-	data = pd.read_csv(data_file_path, sep=",", header=None, names=['text', 'value'])
-	return data
+	data = pandas.read_csv(data_file_path, sep=",", header=None, names=['text', 'label'])
+	return data.head()
+
+def data_for_evaluation(data):
+	train_data, test_data, train_label, test_label = model_selection.train_test_split(data['text'], data['label'])
+	return train_data.head(), test_data.head(), train_label.head(), test_label.head()
+
+def count_vectorize(data, train_data, test_data):
+	# Create a count vectorizer object
+	count_vector = CountVectorizer(analyzer = 'word', token_pattern = r'\w{1,}')
+	count_vector.fit(data['text'])
+
+	# Transform the data
+	train_data_count = count_vector.transform(train_data)
+	test_data_count = count_vector.transform(test_data)
+
+	return train_data_count, test_data_count	
--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 import data.input as input
-import pandas as pd
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+
+import xgboost, numpy, textblob, string
+from keras.preprocessing import text, sequence
+from keras import layers, models, optimizers
 
 print(input.input_file_path())
 
 data = input.structure(input.input_file_path())
 
-print(data)	
+# print(data)	
+
+train_data, test_data, train_label, test_label = input.data_for_evaluation(data)
+
+print(train_data)
+# print(train_label)
+print(test_data)
+# print(test_label)
+
+train_data_count, test_data_count = input.count_vectorize(data, train_data, test_data)
+
+print(train_data_count)
+
+# print(test_data_count)
