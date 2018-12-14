fbbeea10367dab13be0dfb7d8ae62af5332b83a8
alvi2496 <alvi.uvic@gmail.com>
2018-12-14 09:47:04 -0800

    add cross validation parameter

--- app/paradigms/verification/controller.py
+++ app/paradigms/verification/controller.py
@@ -2,7 +2,7 @@ from paradigms.verification.classification import decision_tree, naive_bayes, ra
 from paradigms.verification.regression import logistic_regression
 from paradigms.verification import output, representer
 
-def verify(data):
+def verify(data, test_size_for_split, k_fold_for_cross):
 
 	result = output.initialize()
 
@@ -19,18 +19,18 @@ def verify(data):
 
 	result['cross']['decision_tree']['count_vector']['array'], result['cross']['decision_tree']['count_vector']['mean'], \
 	result['cross']['decision_tree']['tf_idf_vector']['array'], result['cross']['decision_tree']['tf_idf_vector']['mean'] = \
-	decision_tree.cross_verification(data, 10)
+	decision_tree.cross_verification(data, k_fold_for_cross)
 	result['cross']['naive_bayes']['count_vector']['array'], result['cross']['naive_bayes']['count_vector']['mean'], \
 	result['cross']['naive_bayes']['tf_idf_vector']['array'], result['cross']['naive_bayes']['tf_idf_vector']['mean'] = \
-	naive_bayes.cross_verification(data, 10)
+	naive_bayes.cross_verification(data, k_fold_for_cross)
 	result['cross']['random_forest']['count_vector']['array'], result['cross']['random_forest']['count_vector']['mean'], \
 	result['cross']['random_forest']['tf_idf_vector']['array'], result['cross']['random_forest']['tf_idf_vector']['mean'] = \
-	random_forest.cross_verification(data, 10)
+	random_forest.cross_verification(data, k_fold_for_cross)
 	result['cross']['svm']['count_vector']['array'], result['cross']['svm']['count_vector']['mean'], \
 	result['cross']['svm']['tf_idf_vector']['array'], result['cross']['svm']['tf_idf_vector']['mean'] = \
-	svm.cross_verification(data, 10)
+	svm.cross_verification(data, k_fold_for_cross)
 	result['cross']['logistic_regression']['count_vector']['array'], result['cross']['logistic_regression']['count_vector']['mean'], \
 	result['cross']['logistic_regression']['tf_idf_vector']['array'], result['cross']['logistic_regression']['tf_idf_vector']['mean'] = \
-	logistic_regression.cross_verification(data, 10)
+	logistic_regression.cross_verification(data, k_fold_for_cross)
 
 	representer.represent(result)
