c54047224e2bdff92df2e7c07468fbe4c43b93a1
alvi2496 <alvi.uvic@gmail.com>
2019-02-14 21:00:11 -0800

    add word vectorizer

--- app/data/processor.py
+++ app/data/processor.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pandas
 from nltk.corpus import stopwords
 from sklearn import model_selection, preprocessing
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import scale
 
 STOPSET_WORDS = ['might', 'may', 'would', 'must', 'lgtm', 'could', 'can', 'good', 'great', 'nice', 'well', 'better', 'worse', \
 	'worst', 'should', 'i', "i'll", "ill", "it's", "its", "im", "i'm", "they're", "theyre", "you're", "youre", "that's", 'btw', \
@@ -10,6 +12,37 @@ STOPSET_WORDS = ['might', 'may', 'would', 'must', 'lgtm', 'could', 'can', 'good'
 	'jan', 'january', 'feb', 'february', 'mar', 'march', 'apr', 'april', 'may', 'jun', 'june', 'july', 'jul', 'aug', 'august',\
 	'sep', 'september', 'oct', 'october', 'nov', 'novenber', 'dec', 'december', 'pm', 'am'    
 ]
+
+WIKI_WORDS = 'app/data/files/wiki-news-300d-1M.vec'
+encoding="utf-8"
+WORD_DICTIONARY = {}
+print('Loading word-embedding file and making dictionary...')
+for line in open(WIKI_WORDS):
+    values = line.split()
+    WORD_DICTIONARY[values[0]] = np.array(values[1:], dtype='float64')
+
+def normalize(np_array):
+	np_array = scale(np_array, axis=0, with_mean=True, with_std=True, copy=True)
+	min = np_array.min()
+	max = np_array.max()
+	if(max == 0 and min == 0):
+		return np_array
+	return (np_array - min) / (max - min)
+
+def word_embed(data):
+    # load the pre-trained word-embedding vectors 
+    word_vector = np.zeros(np.array((data.shape[0], 300)))
+    i = 0
+    print('embedding word and converting to vector...')
+    for sentence in data:
+        words = sentence.split()
+        # length = len(words)
+        for word in words:
+                if word in WORD_DICTIONARY:
+                        word_vector[i] = np.add(word_vector[i], WORD_DICTIONARY[word])
+        word_vector[i] = normalize(word_vector[i])                
+        i += 1
+    return word_vector
 
 def input_file_path(url):
 	return url.strip()
@@ -37,8 +70,13 @@ def count_vectorize(data, train_data, test_data):
 	count_vector = CountVectorizer(analyzer = 'word', token_pattern = r'\w{1,}')
 	count_vector.fit(data['text'])
 
-	train_data_count = count_vector.transform(train_data)
-	test_data_count = count_vector.transform(test_data)
+	# train_data_count = count_vector.transform(train_data)
+	# test_data_count = count_vector.transform(test_data)
+
+	train_data_count = word_embed(train_data)
+	test_data_count = word_embed(test_data)
+
+	print('word embedding is complete!')
 
 	return train_data_count, test_data_count
 
